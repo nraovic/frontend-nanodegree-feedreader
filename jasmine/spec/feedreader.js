3044b2c1dbb8f45b9f995aec7beb0419c6491d4a
nraovic <n.raovic@gmail.com>
2017-12-09 21:38:58 +0100

    Write Initial Entries test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -80,7 +80,11 @@ $(function() {
         });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe("Initial Entries", function() {
+        it('jhkj', function(){
+            var feed = $('.feed').find(".entry");
+        });
+    });
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
